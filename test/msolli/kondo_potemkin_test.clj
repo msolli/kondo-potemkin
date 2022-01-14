f2cd4b8d224f29660bdd3b2775d22cea5e32a95c
Martin Solli <msolli@gmail.com>
2022-01-14 13:44:42 +0100

    Initial commit

---
+++ test/msolli/kondo_potemkin_test.clj
@@ -0,0 +1,7 @@
+(ns msolli.kondo-potemkin-test
+  (:require [clojure.test :refer :all]
+            [msolli.kondo-potemkin :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
